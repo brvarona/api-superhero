bf9934629d7b4090f436bb0c8c605853ec843543
Braian Varona <braian.varona@coderio.co>
2022-11-13 13:14:15 -0300

    API documentation with swagger

--- src/main/java/com/brvarona/superhero/controller/SuperheroController.java
+++ src/main/java/com/brvarona/superhero/controller/SuperheroController.java
@@ -7,15 +7,22 @@ import org.springframework.http.ResponseEntity;
 import com.brvarona.superhero.model.Superhero;
 import com.brvarona.superhero.payload.SuperheroRequest;
 
+import io.swagger.annotations.ApiOperation;
+
 public interface SuperheroController {
 
+	@ApiOperation(value = "Get all superheros", notes = "Returns a list with all the superheros")
 	ResponseEntity<List<Superhero>> getAllSuperheros(String filter);
 
+	@ApiOperation(value = "Get a superhero by id", notes = "Returns a superhero by id")
 	ResponseEntity<Superhero> getSuperhero(Long id);
 
+	@ApiOperation(value = "Update a superhero", notes = "Returns an updated superhero")
 	ResponseEntity<Superhero> updateSuperhero(Long id, SuperheroRequest superheroRequest);
 
+	@ApiOperation(value = "Remove a superhero", notes = "The selected superhero is removed")
 	ResponseEntity<?> deleteSuperhero(Long id);
 
+	@ApiOperation(value = "Clean superhero cache", notes = "The superhero's cache is cleaned" )
 	ResponseEntity<?> cleanCache();
 }
