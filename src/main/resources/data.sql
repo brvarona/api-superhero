63cf51ab87f07da8bd8a821f192cc350ae3013b3
Braian Varona <braian.varona@coderio.co>
2022-11-13 23:32:23 -0300

    insert superheros in database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO superheros (id, name, power) VALUES (1, 'Capitan America', 'strenght');
+INSERT INTO superheros (id, name, power) VALUES (2, 'Sue Storm', 'invisibility');
+INSERT INTO superheros (id, name, power) VALUES (3, 'Ant man', 'shrink');
+INSERT INTO superheros (id, name, power) VALUES (4, 'Flash', 'speed');
+INSERT INTO superheros (id, name, power) VALUES (5, 'Batman', 'intelligence');
+INSERT INTO superheros (id, name, power) VALUES (6, 'Superman', 'fly');
+INSERT INTO superheros (id, name, power) VALUES (7, 'Spiderman', 'clamber');
+INSERT INTO superheros (id, name, power) VALUES (8, 'Hulk', 'tolerance');
